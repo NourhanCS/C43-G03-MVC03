dd3bab3e26852e71ffc9e6c3f3b13fa3ff418e5d
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-03-16 20:46:27 +0200

    02-Department Module With Configurations

---
+++ IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using IKEA.DAL.Models.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Persistance.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+        public DbSet<Department> Departments { get; set; }
+
+        
+
+    }
+}
