09e390b5d16e0423bbd130969ad972218967f261
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-03-16 19:07:21 +0200

    Add project files.

---
+++ IKEA.PL/Program.cs
@@ -0,0 +1,44 @@
+namespace IKEA.PL
+{
+    public class Program
+    {
+        //Entry Point
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            #region Configure Services 
+            builder.Services.AddControllersWithViews();
+            #endregion
+            
+
+            var app = builder.Build();
+
+            #region Configure Pipelines (MiddleWares)
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+          
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            #endregion
+             
+
+            app.Run();
+        }
+    }
+}
