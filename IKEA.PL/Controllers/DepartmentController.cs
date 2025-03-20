73ccb661259b008dc8b30f081f7f54fb9d4586e1
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-03-21 01:31:25 +0200

    04-Department Create Actions & View

--- IKEA.PL/Controllers/DepartmentController.cs
+++ IKEA.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using IKEA.BLL.Services.DepartmentServices;
+﻿using IKEA.BLL.DTO_S.Departments;
+using IKEA.BLL.Services.DepartmentServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IKEA.PL.Controllers
@@ -7,17 +8,74 @@ namespace IKEA.PL.Controllers
     {//Services => Departments
 
         private IDepartmentServices departmentServices;
+        private readonly ILogger<DepartmentController> logger;
+        private IWebHostEnvironment environment;
 
-        public DepartmentController(IDepartmentServices _departmentServices)
+        public DepartmentController(IDepartmentServices _departmentServices,ILogger<DepartmentController>_logger,IWebHostEnvironment environment)
         {
             departmentServices = _departmentServices;
+            logger = _logger;
+            this.environment = environment;
         }
         #region Index
+        [HttpGet]
         public IActionResult Index()
         {
             var Departments = departmentServices.GetAllDepartments();
             return View(Departments);
-        } 
+        }
         #endregion
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+
+        public IActionResult Create(CreatedDepartmentDto departmentDto)
+        {
+            if(!ModelState.IsValid)
+                return View(departmentDto);
+
+            var Message=string.Empty;
+            try
+            {
+                var Result = departmentServices.CreateDepartment(departmentDto);
+                if (Result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                {
+                    Message = "Department is not Created";
+                    ModelState.AddModelError(string.Empty, Message);
+                    return View(departmentDto);
+                }
+            }
+            catch (Exception ex)
+            {
+                //1.log Exception Kestral
+               logger.LogError(ex,ex.Message);
+
+                //2.Set Default Message To User
+                if(environment.IsDevelopment())
+                {
+                    Message=ex.Message;
+                    ModelState.AddModelError(string.Empty, Message);
+                    return View(departmentDto);
+                }
+                else
+                {
+                    Message = "An Error Effect in The Creation Operator";
+                    ModelState.AddModelError(string.Empty, Message);
+                    return View(departmentDto);
+                }
+
+            }
+            
+
+           
+        }
     }
 }
