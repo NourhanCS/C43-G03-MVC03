a85b9a3551adb1f631a900ce8a6bd27f850da979
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-03-30 02:36:06 +0200

    05- Employees Services & Dto's

---
+++ IKEA.BLL/DTO'S/Employees/EmployeeDto.cs
@@ -0,0 +1,22 @@
+﻿using IKEA.DAL.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.BLL.DTO_S.Employees
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int? Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+        public string? Email { get; set; }
+        public string Gender { get; set; } = null!;
+        public string EmployeeType { get; set; } = null!;
+    }
+}
---
+++ IKEA.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using IKEA.BLL.Services.EmployeeServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKEA.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        #region Services - DI
+        private readonly IEmployeeServices employeeServices;
+        private readonly ILogger<EmployeeController> logger;
+        private readonly IWebHostEnvironment environment;
+
+        public EmployeeController(IEmployeeServices employeeServices, ILogger<EmployeeController> logger, IWebHostEnvironment environment)
+        {
+            this.employeeServices = employeeServices;
+            this.logger = logger;
+            this.environment = environment;
+        }
+
+        #endregion
+
+        #region Index 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
