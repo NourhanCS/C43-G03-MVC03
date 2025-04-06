4232e959ce0b30b256c18eaf632c034da7db2bbc
NourhanCS <norhanmohmoud.2004@gmail.com>
2025-04-06 04:17:20 +0200

    02- Employee Create

--- IKEA.PL/Controllers/EmployeeController.cs
+++ IKEA.PL/Controllers/EmployeeController.cs
@@ -1,4 +1,7 @@
-﻿using IKEA.BLL.Services.EmployeeServices;
+﻿using IKEA.BLL.DTO_S.Departments;
+using IKEA.BLL.DTO_S.Employees;
+using IKEA.BLL.Services.DepartmentServices;
+using IKEA.BLL.Services.EmployeeServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IKEA.PL.Controllers
@@ -28,6 +31,48 @@ namespace IKEA.PL.Controllers
             return View(Employees);
         }
 
+        #endregion
+
+        #region Create
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(CreatedEmployeeDto EmployeeDto)
+        {
+            if (!ModelState.IsValid)
+                return View(EmployeeDto);
+
+            var Message = string.Empty;
+            try
+            {
+                var Result = employeeServices.CreateEmployee(EmployeeDto);
+                if (Result > 0)
+                    return RedirectToAction(nameof(Index));
+                else
+                    Message = "Department is not Created";
+            }
+            catch (Exception ex)
+            {
+                //1.log Exception Kestral
+                logger.LogError(ex, ex.Message);
+
+                //2.Set Default Message To User
+                if (environment.IsDevelopment())
+                    Message = ex.Message;
+                else
+                    Message = "An Error Effect in The Creation Operator";
+
+            }
+            ModelState.AddModelError(string.Empty, Message);
+            return View(EmployeeDto);
+        }
         #endregion
     }
 }
